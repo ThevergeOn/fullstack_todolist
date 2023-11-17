d535047211a60aa9d7e1507a7ac3143551e2a5e1
Zahra <barqamadizahra@gmail.com>
2023-11-17 22:09:19 +0330

    db utils

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -3,6 +3,7 @@ import CredentialsProvider from "next-auth/providers/credentials";
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import { db } from "./db";
 import { compare } from "bcrypt";
+
 export const authOptions: NextAuthOptions = {
   adapter: PrismaAdapter(db),
   session: {
@@ -10,19 +11,11 @@ export const authOptions: NextAuthOptions = {
   },
   pages: {
     signIn: "/sign-in",
-    // error: "/login",
-    // verifyRequest: "/login",
-    // newUser: "/register",
   },
-  // Configure one or more authentication providers
   providers: [
     CredentialsProvider({
-      // The name to display on the sign in form (e.g. "Sign in with...")
+      id:"username-login",
       name: "Credentials",
-      // `credentials` is used to generate a form on the sign in page.
-      // You can specify which fields should be submitted, by adding keys to the `credentials` object.
-      // e.g. domain, username, password, 2FA token, etc.
-      // You can pass any HTML attribute to the <input> tag through the object.
       credentials: {
         email: {
           label: "Email",
@@ -31,17 +24,20 @@ export const authOptions: NextAuthOptions = {
         },
         password: { label: "Password", type: "password" },
       },
+
       async authorize(credentials) {
-        // Add logic here to look up the user from the credentials supplied
+
         if (!credentials?.email || !credentials?.password) {
           return null;
         }
+
         const existingUser = await db.user.findUnique({
-          where: { email: credentials.email },
+          where: { email: credentials?.email },
         });
         if (!existingUser) {
           return null;
         }
+
         const passwordIsMatch = await compare(
           credentials.password,
           existingUser.password
@@ -51,7 +47,7 @@ export const authOptions: NextAuthOptions = {
         }
         return {
             id: `${existingUser.id}`,
-            username:existingUser.userName,
+            username:existingUser.username,
             email:existingUser.email
         };
       },
--- src/lib/db.ts
+++ src/lib/db.ts
@@ -9,4 +9,5 @@ const prisma = globalForPrisma.prisma ?? new PrismaClient();
 export default prisma;
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+
 export const db = prisma;
